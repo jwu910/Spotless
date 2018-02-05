992a558456c38e90a241c7e377ad42d677a06f05
Joshua Wu <Joshua.wu.0910@gmail.com>
2018-02-05 14:50:33 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const client_secret = require('./.client_secret');
+iconst client_id = '35e5b7917b064414b7dc384e07d6dab8'; // Your client id
+const redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
+
+
